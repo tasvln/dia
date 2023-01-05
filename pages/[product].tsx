339c39d7146ca3a45a8009178caed28b7885c318
Adebayo Temitope <tasxzd@gmail.com>
2023-01-04 21:15:49 -0500

    ske

---
+++ pages/[product].tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { styled } from '@stitches/react';
+
+export default function Product() {
+  return (
+    <div>
+      <Head>
+        <title>Dia Cart - Shopify Test</title>
+      </Head>
+    </div>
+  )
+}
---
+++ pages/cart.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { styled } from '@stitches/react';
+
+export default function Cart() {
+  return (
+    <div>
+      <Head>
+        <title>Dia Cart - Shopify Test</title>
+      </Head>
+    </div>
+  )
+}
